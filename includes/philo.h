9137f20b03a6cd1570a80123d7aa52be20c8afc7
dklimkin <d.klimkin@icloud.com>
2024-01-23 23:44:54 +0700

    feat: add time_to_die counter

--- includes/philo.h
+++ includes/philo.h
@@ -17,10 +17,22 @@
 # include <pthread.h>
 # include <limits.h>
 # include <stdbool.h>
+# include <sys/time.h>
+
+# define MS_PER_SEC 1000
+# define US_PER_MS 1000
 
 # define DEFAULT_ERR_CTX "Error"
 # define NOT_ENOUGH_ARGS_ERR "Not enough arguments provided\n"
 # define INVALID_ARGS_ERR "Provided arguments are not valid\n"
+
+typedef struct s_philo
+{
+	int				id;
+	long			time_to_die;
+	long			time_to_eat;
+	struct timeval	start_time;
+}	t_philo;
 
 int	ft_atoi(const char *str);
 
--- src/main.c
+++ src/main.c
@@ -23,25 +23,45 @@ int	handle_error(char *err_ctx, char *err_msg)
 
 int	is_argv_valid(int argc, char **argv)
 {
-	int	i;
-
-	i = 0;
-	while (i < argc)
+	while (argc--)
 	{
-		if (ft_atoi(argv[i]) == 0)
+		if (ft_atoi(argv[argc]) <= 0)
 			return (false);
-		i++;
 	}
 	return (true);
 }
 
+int	is_philosopher_dead(struct timeval start_time, long time_to_die)
+{
+	struct timeval	current_time;
+	long			elapsed_time;
+
+	gettimeofday(&current_time, NULL);
+	elapsed_time = (current_time.tv_sec - start_time.tv_sec) * MS_PER_SEC;
+	elapsed_time += (current_time.tv_usec - start_time.tv_usec) / US_PER_MS;
+	if (elapsed_time >= time_to_die)
+	{
+		printf("Philosopher X died\n");
+		return (true);
+	}
+	return (false);
+}
+
 int	main(int argc, char **argv)
 {
-	pthread_t	*threads;
+	struct timeval	start_time;
+	long			time_to_die;
 
-	if (argc != 6)
+	if (argc < 5)
 		handle_error(NULL, NOT_ENOUGH_ARGS_ERR);
 	if (is_argv_valid((argc - 1), (argv + 1)) == false)
 		handle_error(NULL, INVALID_ARGS_ERR);
+	gettimeofday(&start_time, NULL);
+	time_to_die = ft_atoi(argv[2]);
+	while (1)
+	{
+		if (is_philosopher_dead(start_time, time_to_die))
+			return (EXIT_SUCCESS);
+	}
 	return (EXIT_SUCCESS);
 }
